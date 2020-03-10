a128b12c9007e01578f4823cf323922bbaaa806b
Vladislav Osin <osinvladislav@yandex.ru>
2020-03-10 13:13:23 +0300

    Initial Commit

---
+++ MemorablePearls/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MemorablePearls
+//
+//  Created by Vladislav Osin on 10.03.2020.
+//  Copyright © 2020 Splash studio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
